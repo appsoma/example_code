b0992ba70a9989ab0d9e2c370cc5bb79556c782e
Zack Simpson <zack.simpson@gmail.com>
2015-06-12 11:05:22 -0500

    code in sheep service

--- count_sheep_service.py
+++ count_sheep_service.py
@@ -1,1 +1,21 @@
-# This will count sheep
+import sys
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+
+if sys.argv[1:]:
+    port = int(sys.argv[1])
+else:
+    port = 8900
+
+class Handler(BaseHTTPRequestHandler):
+	def do_GET(self):
+		self.send_response(200)
+		self.send_header('Content-type','text/html')
+		self.end_headers()
+		self.wfile.write( "The service is up" )
+
+
+httpd = HTTPServer( ('0.0.0.0', port), Handler )
+
+sa = httpd.socket.getsockname()
+print "Serving HTTP on", sa[0], "port", sa[1], "..."
+httpd.serve_forever()
