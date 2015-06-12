7728d6bfa3abf9a118d1f8f9bf5e9c46059f02b6
Zack Simpson <zack.simpson@gmail.com>
2015-06-12 12:48:22 -0500

    Work on service

--- web_service.py
+++ web_service.py
@@ -32,7 +32,7 @@ class Handler(BaseHTTPRequestHandler):
 		self.send_response(200)
 		self.send_header('Content-type','text/html')
 		self.end_headers()
-		self.wfile.write( "The service is up. The parameter 'sheep_count' is:"+params['sheep_count'] )
+		self.wfile.write( "The service is up. The parameter 'to_print' is:"+params['to_print'] )
 
 
 httpd = HTTPServer( ('0.0.0.0', port), Handler )
